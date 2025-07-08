644b8db407db536b7f3bf6c07c83e49455991938
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-08 17:30:35 -0600

    updated dog model with decorator, added vets, updated checkups to a list of dicionaries, and added new methods to add a new checkup and find a checkup

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,46 @@
 class Dog:
-  def __init__(self, name, breed, age, last_checkup = None):
+  def __init__(self, name, breed, age):
     self.name = name
     self.breed = breed
     self.age = age
-    self.last_checkup = last_checkup
+    self.vets = []
+    self.checkups = []
 
-  def get_age(self):
+  @property
+  def age(self):
     return self._age
-  def set_age(self,value):
+  
+  @age.setter
+  def age(self,value):
     if type(value) is int and 0 <= value:
       self._age = value
     else:
-      print("Not valid age")
-  age = property(get_age,set_age)
+      raise ValueError("Not valid age")
+    
+  def add_checkup(self, vet, date, notes):
+    if vet not in self.vets:
+      self.vets.append(vet)
+    new_checkup = {
+      "vet": vet,
+      "date": date,
+      "notes": notes
+    }
+    self.checkups.append(new_checkup)
+
+  def find_checkup(self, date):
+    for checkup in self.checkups:
+      if checkup["date"] == date:
+        print(f"Checkup on {date} by {checkup["vet"]}: {checkup["notes"]}")
+        return print(f"No checkup found on {date}")
+        
+fido = Dog(
+   name = "Fido",
+   age = 3, 
+   breed = "Golden Retriever"
+   )
+fido.add_checkup("Doolittle", "02/20/22","Good health!")
+print(fido.vets)
+print(fido.checkups)
+fido.find_checkup("02/20/22")
+fido.find_checkup("03/22/24")        
+  
